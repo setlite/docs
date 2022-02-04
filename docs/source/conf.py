8afcfc898420cf3e34733497aadc560507c47cfa
setlite <98998073+setlite@users.noreply.github.com>
2022-02-04 10:02:31 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Webed'
+copyright = '2022, SetLite'
+author = 'Acroner'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.0.1'
 
 # -- General configuration
 
